752ed6901c351d116a14567a4bd60baaea41677b
yunchej <jiangyuncheng@glb-auto.com>
2020-07-31 23:09:41 +0800

    add leetcode offer 14

---
+++ LeetCode/leetcode/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <iostream>
+#include"offer14.h"
+using namespace std;
+
+int main()
+{
+    int n = 7;
+    Solution sol(n);
+    int max = sol.cuttingRope(n);
+    cout<<max<<endl;
+    return 0;
+}
---
+++ LeetCode/leetcode/offer14.h
@@ -0,0 +1,7 @@
+#include <iostream>
+class Solution{
+public:
+    Solution(int){};
+    ~Solution(){};
+    int cuttingRope(int);
+};
